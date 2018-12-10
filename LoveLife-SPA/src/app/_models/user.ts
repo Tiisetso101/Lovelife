eff3aa80b80cc8fa1c9c3ce9ab06132540be24cf
TIISETSO\TiisetsoM <tiisetso88@live.com>
2018-12-10 16:27:10 +0200

    updated site to ensure member's photos appear on the website

--- LoveLife-SPA/src/app/_models/user.ts
+++ LoveLife-SPA/src/app/_models/user.ts
@@ -8,7 +8,7 @@ export interface User {
     gender: string;
     created: Date;
     lastActive: Date;
-    photourl: string;
+    photoUrl: string;
     city: string;
     country: string;
     interests?: string;
--- LoveLife-SPA/src/app/_services/user.service.ts
+++ LoveLife-SPA/src/app/_services/user.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../_models/user';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+ headers: new HttpHeaders({
+   'Authorization': 'Bearer ' + localStorage.getItem('token'),
+ })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +16,14 @@ import {environment} from '../../environments/environment';
 export class UserService {
   baseUrl =  environment.apiUrl;
 
-constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable <User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users/', httpOptions);
+  }
+
+  getUser(id): Observable <User[]> {
+    return this.http.get<User[]>(this.baseUrl + 'users/' + id, httpOptions);
+  }
 
 }
