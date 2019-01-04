554ba403c7e6eed03afea2ccd49f7abae7a8fd51
TIISETSO\TiisetsoM <tiisetso88@live.com>
2019-01-04 21:38:57 +0200

    Added photo editor component

--- LoveLife-SPA/src/app/app.module.ts
+++ LoveLife-SPA/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import {PhotoEditorComponent} from './members/photo-editor/photo-editor.component';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -43,7 +44,8 @@ export function tokenGetter() {
       MessagesComponent,
       MemberCardComponent,
       MemberDetailComponent,
-      MemberEditComponent
+      MemberEditComponent,
+      PhotoEditorComponent
    ],
    imports: [
       BrowserModule,
